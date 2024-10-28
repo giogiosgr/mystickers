c5d062fe10fcba9c6588a176395e9c9a95ddfda6
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-28 22:19:12 +0100

    feat: functions, index page

--- src/main/java/org/mystickers/java/controller/StickerController.java
+++ src/main/java/org/mystickers/java/controller/StickerController.java
@@ -1,5 +1,26 @@
 package org.mystickers.java.controller;
 
+import org.mystickers.java.service.StickerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stickers")
 public class StickerController {
+	
+	@Autowired
+	StickerService stickerService;
+	
+	// INDEX
+	@GetMapping()
+	public String index(Model model) {
+
+		model.addAttribute("stickers", stickerService.getAllOrderedByCreatedAt());
+
+		return "stickers/index";
+	}
 
 }
--- src/main/java/org/mystickers/java/model/Sticker.java
+++ src/main/java/org/mystickers/java/model/Sticker.java
@@ -1,5 +1,85 @@
 package org.mystickers.java.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "stickers")
 public class Sticker {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 2, max = 5000)
+	@Column(name = "text", nullable = false, columnDefinition = "text")
+	private String text;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	@Transient
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy' 'HH:mm");
+	
+	// getters and setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	
+	public String getFormattedCreatedAt() {
+		return createdAt.format(formatter);
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public String getFormattedUpdatedAt() {
+		return updatedAt.format(formatter);
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 
 }
