031cf76564215a45705b31b462d1c973039c32dc
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-28 22:57:09 +0100

    feat: stickers crud

--- src/main/java/org/mystickers/java/controller/StickerController.java
+++ src/main/java/org/mystickers/java/controller/StickerController.java
@@ -1,19 +1,28 @@
 package org.mystickers.java.controller;
 
+import org.mystickers.java.model.Sticker;
 import org.mystickers.java.service.StickerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/stickers")
 public class StickerController {
-	
+
 	@Autowired
 	StickerService stickerService;
-	
+
 	// INDEX
 	@GetMapping()
 	public String index(Model model) {
@@ -21,6 +30,67 @@ public class StickerController {
 		model.addAttribute("stickers", stickerService.getAllOrderedByCreatedAt());
 
 		return "stickers/index";
+	}
+
+	// CREATE
+	@GetMapping("/create")
+	public String create(Model model) {
+
+		model.addAttribute("sticker", new Sticker());
+
+		return "stickers/create";
+	}
+
+	// STORE
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("sticker") Sticker stickerForm, BindingResult bindingResult, Model model,
+			RedirectAttributes attributes) {
+
+		if (bindingResult.hasErrors()) {
+			return "stickers/create";
+		}
+
+		stickerService.save(stickerForm);
+
+		attributes.addFlashAttribute("successMessage", "new sticker added");
+
+		return "redirect:/stickers";
+	}
+
+	// EDIT
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model model) {
+
+		model.addAttribute("sticker", stickerService.getById(id));
+
+		return "stickers/edit";
+	}
+
+	// UPDATE
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute("sticker") Sticker stickerForm, BindingResult bindingResult,
+			Model model, RedirectAttributes attributes) {
+
+		if (bindingResult.hasErrors()) {
+			return "stickers/edit";
+		}
+
+		stickerService.save(stickerForm);
+
+		attributes.addFlashAttribute("successMessage", "sticker edited");
+
+		return "redirect:/stickers";
+	}
+
+	// DELETE
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable int id, RedirectAttributes attributes) {
+
+		stickerService.deleteById(id);
+
+		attributes.addFlashAttribute("successMessage", "sticker deleted");
+
+		return "redirect:/stickers";
 	}
 
 }
--- src/main/java/org/mystickers/java/service/StickerService.java
+++ src/main/java/org/mystickers/java/service/StickerService.java
@@ -7,15 +7,35 @@ import org.mystickers.java.repo.StickerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class StickerService {
 	
 	@Autowired
 	StickerRepository stickerRepo;
 	
+	public Sticker getById(Integer id) {
+
+		return stickerRepo.findById(id).get();
+
+	}
+	
 	public List<Sticker> getAllOrderedByCreatedAt() {
 
 		return stickerRepo.findAllOrderByCreatedAtDesc();
+
+	}
+
+	public void save(@Valid Sticker stickerForm) {
+		
+		stickerRepo.save(stickerForm);
+		
+	}
+	
+	public void deleteById(int id) {
+
+		stickerRepo.deleteById(id);
 
 	}
 
