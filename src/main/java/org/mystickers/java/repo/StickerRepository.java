8ea182adc6c50fe9e518fccd441a151b079c7a5f
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-28 21:54:20 +0100

    files

--- src/main/java/org/mystickers/java/repo/StickerRepository.java
+++ src/main/java/org/mystickers/java/repo/StickerRepository.java
@@ -1,5 +1,14 @@
 package org.mystickers.java.repo;
 
-public interface StickerRepository {
+import java.util.List;
+
+import org.mystickers.java.model.Sticker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StickerRepository extends JpaRepository<Sticker, Integer>{
+	
+	List<Sticker> findAllOrderByCreatedAtDesc();
 
 }
--- src/main/java/org/mystickers/java/service/StickerService.java
+++ src/main/java/org/mystickers/java/service/StickerService.java
@@ -1,5 +1,22 @@
 package org.mystickers.java.service;
 
+import java.util.List;
+
+import org.mystickers.java.model.Sticker;
+import org.mystickers.java.repo.StickerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StickerService {
+	
+	@Autowired
+	StickerRepository stickerRepo;
+	
+	public List<Sticker> getAllOrderedByCreatedAt() {
+
+		return stickerRepo.findAllOrderByCreatedAtDesc();
+
+	}
 
 }
